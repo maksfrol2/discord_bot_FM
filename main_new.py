c798076bc4071bee7321a20afcb4d11079ef6efc
maksfrol2 <79906671+maksfrol2@users.noreply.github.com>
2021-12-18 06:58:05 +0200

    fixes

--- main_new.py
+++ main_new.py
@@ -2,18 +2,20 @@ import discord
 from discord.ext import commands
 from music_new import Player
 
-intents = discord.Intents.default() #Устанавливает необходимый доступ
+intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents) #Задается командный префикс бота и права доступа
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} is ready") #Сообщает об успешном запуске бота
+    '''При вызове функция выводит сообщение о готовности бота'''
+    print(f"{bot.user.name} is ready")
 
-async def setup(): #Первоначальная настройка
+async def setup():
+    '''Начальная настройка'''
     await bot.wait_until_ready() 
     bot.add_cog(Player(bot)) 
 
 bot.loop.create_task(setup())
-bot.run('OTE3NDg2MjczOTkwNjUxOTE0.Ya5ZrQ.M7adMTwuoCdWh-9Jb_CqPymN8cc') #старт бота, в скобках указывается токен
+bot.run('OTE3NDg2MjczOTkwNjUxOTE0.Ya5ZrQ.M7adMTwuoCdWh-9Jb_CqPymN8cc') # в скобках указывается токен
--- music_new.py
+++ music_new.py
@@ -1,66 +1,103 @@
-
 import discord
-from discord import message
 from discord.ext.commands.core import command
 import pafy
 import random
 from discord.ext import commands
 
-class Player(commands.Cog): #инициализация класса
+class Player(commands.Cog):
+    '''Класс, содержащий в себе все функции, необходимые для работы музыукального бота'''
     def __init__(self, bot):
         self.bot = bot 
 
-    async def play_song(self, ctx, song:str):
+    async def play_song(self, ctx: commands.Context, song:str):
+        '''Функция ответственная за проигрывание музыки.
+        Принимает на вход контекст и строку с url ссылкой'''
         url = pafy.new(song).getbestaudio().url
-        ctx.voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url))) #Проигрывает аудио
-        ctx.voice_client.source.volume = 1 #Громкость воспроизведения произведения
+        ctx.voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url)))
+        ctx.voice_client.source.volume = 1 #Громкость воспроизведения
 
     @commands.command()
-    async def join(self, ctx):
-        if ctx.author.voice is None: #Проверяет, находится ли автор команды в голосовом канале
+    async def join(self, ctx:commands.Context):
+        '''При вызове подключает бота в голосовой канал к пользователю, который отправил команду.
+        Принимает на вход контекст'''
+        if ctx.author.voice is None:
             return await ctx.send("Вы не находитесь в голосовом канале!")
-        elif ctx.voice_client is not None: 
+        elif ctx.voice_client is not None and ctx.voice_client.channel != ctx.author.voice.channel:
             await ctx.voice_client.disconnect()
         await ctx.author.voice.channel.connect()
         
     @commands.command()
-    async def leave(self, ctx):
+    async def leave(self, ctx:commands.Context):
+        '''При вызове отключается от голосового канала
+        Принимает на вход контекст'''
         if ctx.voice_client is not None: 
             await ctx.voice_client.disconnect()
 
     @commands.command()
-    async def play(self, ctx, song):
+    async def play(self, ctx:commands.Context, song : str):
+        '''При вызове начинает проигрывать музыку.
+        Если бот заранее не был добавлен, то он подключится
+        Принимает на вход контекст.'''
         if ctx.author.voice is None: 
-            return await ctx.send("Вы не находитесь в голосовом канале!")   
-        await self.play_song(ctx, song)
-        await ctx.send("Воспроизвожу")
+            return await ctx.send("Вы не находитесь в голосовом канале!")
+        if ctx.voice_client is not None and ctx.voice_client.channel == ctx.author.voice.channel:
+            embed=discord.Embed(title='Воспроизведение')
+            await ctx.send(embed=embed)
+            await self.play_song(ctx, song)
+        else:
+            await ctx.author.voice.channel.connect()
+            await self.play(ctx,song)
 
     @commands.command()
-    async def pause(self, ctx):
+    async def reset(self, ctx:commands.Context):
+        '''При вызове бот перезаходит в текущий голосовой канал.
+        Принимает на вход контекст'''
+        if ctx.author.voice is None:
+            return await ctx.send("Вы не находитесь в голосовом канале!")
+        elif ctx.voice_client is not None and ctx.voice_client.channel == ctx.author.voice.channel:
+            await ctx.voice_client.disconnect()
+            await ctx.author.voice.channel.connect()
+
+    @commands.command()
+    async def pause(self, ctx:commands.Context):
+        '''Ставит на паузу. Вполне очевидно.
+        Принимает на вход контекст
+        '''
+        embed=discord.Embed(title='Пауза')
+        await ctx.send(embed=embed)
         await ctx.voice_client.pause()
-        await ctx.send('Музыка поставлена на паузу')
 
     @commands.command()
-    async def resume(self, ctx):
+    async def resume(self, ctx:commands.Context):
+        '''Возобновляет воспроизведение.
+        Принимает на вход контекст'''
+        embed=discord.Embed(title='Воспроизведение')
+        await ctx.send(embed=embed)
         await ctx.voice_client.resume()
-        await ctx.send('Музыка снова проигрывается')
-
+        
     @commands.command()
-    async def rr(self,ctx):
+    async def rr(self,ctx:commands.Context):
+        '''При вызове с вероятностью 1 к 6 может начать проигрывать одну
+        надоедливую песню из 1987 года.
+        Принимает на вход контекст'''
         if ctx.author.voice is None: 
             return await ctx.send("Вы не находитесь в голосовом канале!")
         if random.randrange(6) == 1:  
             await self.play_song(ctx, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-            await ctx.send("Воспроизвожу")
-        else:
             await ctx.send("Повезло, повезло")
+        else:
+            await ctx.send("Nope")
 
     @commands.command()
-    async def commands(self,ctx):
+    async def commands(self,ctx:commands.Context):
+        '''При вызове выдает список всех доступных команд
+        Принимает на вход контекст'''
         embed=discord.Embed(title='Список команд')
         embed.add_field(name = '!play', value="Воспроизведение музыки по ссылке, укажите ссылку через пробел после команды", inline = True)
         embed.add_field(name = '!pause', value = "Пауза",inline=False)
         embed.add_field(name = '!resume', value = "Повторное воспроизведение", inline=False)
+        embed.add_field(name = '!reset', value = "Сброс текущей песни", inline=False)
         embed.add_field(name = '!join', value = "Подключение к голосовому каналу", inline =False)
         embed.add_field(name = '!leave', value= "Отключение от голосового канала", inline = False)
+        embed.add_field(name = '!rr', value = "Русская рулетка", inline =False)
         await ctx.send(embed=embed)
